c78ed3538adf0f54543802643e95f615f0f2f151
Kenny <kenny__jing@163.com>
2017-02-22 22:50:33 +0800

    AOP实现日志管理

---
+++ src/io/vicp/goradical/surveypark/service/LogService.java
@@ -0,0 +1,10 @@
+package io.vicp.goradical.surveypark.service;
+
+import io.vicp.goradical.surveypark.model.Log;
+
+/**
+ * 日志Service
+ */
+public interface LogService extends BaseService<Log>{
+
+}
