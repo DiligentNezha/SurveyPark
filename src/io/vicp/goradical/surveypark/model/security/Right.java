afb41766bca38a7f9a7075906c08b06f2f9239b2
Kenny <kenny__jing@163.com>
2017-03-23 08:14:41 +0800

    update

--- src/io/vicp/goradical/surveypark/model/security/Right.java
+++ src/io/vicp/goradical/surveypark/model/security/Right.java
@@ -2,6 +2,8 @@ package io.vicp.goradical.surveypark.model.security;
 
 import io.vicp.goradical.surveypark.model.BaseEntity;
 
+import javax.persistence.Column;
+
 /**
  * 权限
  */
@@ -9,12 +11,19 @@ public class Right extends BaseEntity{
 	private static final long serialVersionUID = 1885811947450336293L;
 
 	private Integer id;
+
 	private String rightName = "未命名";
+
 	private String rightUrl;
+
 	private String rightDesc;
+
 	private long rightCode;// 权限码,1<<n
+
 	private int rightPos; // 权限位,相当于对权限分组,从0开始
 	//是否是公共资源
+
+	@Column()
 	private boolean common;
 
 	public Integer getId() {
